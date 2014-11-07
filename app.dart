2bbfbece6221e6a5445cd43e3bc7a72ccc1fc926
admix <admix.snurnikov@gmail.com>
2014-11-06 20:48:26 -0500

    Simple dart app

---
+++ app.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+main() {
+  HttpServer.bind(InternetAddress.LOOPBACK_IP_V4, 8080)
+      .then((HttpServer server) {
+    print('listening on localhost, port ${server.port}');
+    server.listen((HttpRequest request) {
+      request.response.write('Hello, world!');
+      request.response.close();
+    });
+  }).catchError((e) => print(e.toString()));
+}
